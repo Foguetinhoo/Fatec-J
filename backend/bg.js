4e93bc6cfd9974119a1b5c8faa91a7d551c7c006
ThiagoFigueiredoFilho <thiagofigueiredofilho@gmail.com>
2025-11-01 23:32:58 -0300

    adicionei o scroll e a leitura temporal

--- backend/bg.js
+++ backend/bg.js
@@ -1,9 +1,22 @@
 
 const readerarea = document.getElementById("reader-area")
+const savedText = localStorage.getItem("savedPDFtext")
+const savedscrollpos = localStorage.getItem("scrollpos")
+const nspos = parseInt(savedscrollpos)
+
+if (savedText) {
+    readerarea.innertext = savedText;
+    readerarea.scrollTop = nspos;
+}
 
 readerarea.addEventListener("blur", function(){
     const textoatual = readerarea.innertext
     localStorage.setItem("savedPDFtext", textoatual);
+
 });
 
+readerarea.addEventListener("scroll", function(){
+    const scrollpos = readerarea.scrollTop
+    localStorage.setItem("scrollpos", nspos)
+})
 
